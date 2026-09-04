4c9cf618382750b4e60a2268edd7fb841182bfdb
cmgabriel01 <cmgabrielhenrique.alecrim@gmail.com>
2026-09-04 19:43:20 -0300

    deposito

--- main_banco.py
+++ main_banco.py
@@ -1,1 +1,14 @@
+#Função para realizar depósito
+def realizar_deposito(saldo):
+    valor = float(input("Valor do depósito: R$ "))
+    if valor > 0:
+        saldo = saldo + valor
+        print("Depósito realizado!")
+        print(f"Saldo atual: R${saldo:.2f}")
+    else:
+        print("Valor inválido.")
+    return saldo
 
+#parte para o sistema de menu
+elif opcao == 4:
+        saldo = realizar_deposito(saldo)
